e3d51f864f7e238a5d7a12e67cfde714a8386b43
Jamu Kakar <jkakar@kakar.ca>
2013-10-20 22:36:50 -0700

    - Wired up links in resources.

--- lib/heroics/resource.rb
+++ lib/heroics/resource.rb
@@ -2,8 +2,10 @@ module Heroics
   # A representation of a resource that exposes links as methods.
   class Resource
     # Instantiate a resource.
-    def initialize(name, methods)
-      @name = name
+    #
+    # @param methods [Hash<String,Link>] A hash that maps method names to
+    #   links.
+    def initialize(methods)
       @methods = methods
     end
 
@@ -12,19 +14,18 @@ module Heroics
     # Make an HTTP request to the endpoint matching the specified link.
     #
     # @param name [String] The name of the method to invoke.
-    # @param args [Array] The arguments to pass to the method.  This should
-    #   always be a `Hash` mapping parameter names to values.
+    # @param parameters [Array] The arguments to pass to the method.  This
+    #   should always be a `Hash` mapping parameter names to values.
     # @raise [NoMethodError] Raised if the name doesn't match a known method.
     # @return [String,Array,Hash] The response received from the server.  JSON
     #   responses are automatically decoded into Ruby objects.
-    def method_missing(name, *args)
-      method = @methods[name]
-      if method.nil?
+    def method_missing(name, *parameters)
+      link = @methods[name.to_s]
+      if link.nil?
         address = "<#{self.class.name}:0x00#{(self.object_id << 1).to_s(16)}>"
         raise NoMethodError.new("undefined method `#{name}' for ##{address}")
       end
-      options = args.first
-      method.run(options)
+      link.run(*parameters)
     end
   end
 end
--- test/resource_test.rb
+++ test/resource_test.rb
@@ -3,20 +3,32 @@ require 'helper'
 class ResourceTest < MiniTest::Test
   include ExconHelper
 
-  def setup
-    super
-    @resource = Heroics::Resource.new('resource', SAMPLE_SCHEMA)
-  end
-
   # Resource.<method> raises a NoMethodError when a method is invoked without
   # a matching link.
   def test_invalid_method
-    resource = Heroics::Resource.new('resource', [])
+    resource = Heroics::Resource.new({})
     error = assert_raises NoMethodError do
-      @resource.unknown
+      resource.unknown
     end
     assert_match(
       /undefined method `unknown' for #<Heroics::Resource:0x[0-9a-f]{14}>/,
       error.message)
+  end
+
+  # Resource.<method> finds the appropriate link and invokes it.
+  def test_method
+    link = Heroics::Link.new('https://username:secret@example.com',
+                             '/resource', :get)
+    resource = Heroics::Resource.new({'link' => link})
+    Excon.stub(method: :get) do |request|
+      assert_equal('Basic dXNlcm5hbWU6c2VjcmV0',
+                   request[:headers]['Authorization'])
+      assert_equal('example.com', request[:host])
+      assert_equal(443, request[:port])
+      assert_equal('/resource', request[:path])
+      Excon.stubs.pop
+      {status: 200, body: 'Hello, world!'}
+    end
+    assert_equal('Hello, world!', resource.link)
   end
 end
