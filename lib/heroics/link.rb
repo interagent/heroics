35c54d900da3d84a50d047c07b22eaa90a50dea7
Jamu Kakar <jkakar@kakar.ca>
2013-10-20 22:13:47 -0700

    - Basic link functionality works.

--- lib/heroics/link.rb
+++ lib/heroics/link.rb
@@ -2,17 +2,41 @@ module Heroics
   # A representation of a link that can be invoked to make a request to a
   # service and return a response.
   class Link
+    # Instantiate a link.
+    #
+    # @param url [String] The URL to use when making requests.  Include the
+    #   username and password to use with HTTP basic auth.
+    # @param path [String] The path to use when making requests.  Substrings
+    #   that match `{(#/name)}` are replaced with user provided values when
+    #   the link is invoked.
+    # @param method [Symbol] A symbol representing the HTTP method to use when
+    #   invoking the link.
     def initialize(url, path, method)
       @url = url
       @path = path
       @method = method
     end
 
+    # Make a request to the server.
+    #
+    # @param parameters [Array] The list of parameters to inject into the
+    #   path.  A request body can be passed as the final parameter and will
+    #   always be converted to JSON before being transmitted.
+    # @raise [ArgumentError] Raised if either too many or too few parameters
+    #   were provided.
+    # @return [String,Object] A string for text responses or an object for
+    #   JSON responses.
     def run(*parameters)
       path, body = format_path(parameters)
       connection = Excon.new(@url)
-      response = connection.get(path: path)
-      if response.headers['Content-Type'] == 'application/json;charset=utf-8'
+      if body
+        headers = {'Content-Type' => 'application/json'}
+        body = MultiJson.dump(body)
+      end
+      response = connection.request(method: @method, path: path,
+                                    headers: headers, body: body)
+      content_type = response.headers['Content-Type']
+      if content_type && content_type.include?('application/json')
         MultiJson.load(response.body)
       elsif !response.body.empty?
         response.body
@@ -21,18 +45,35 @@ module Heroics
 
     private
 
+    # Inject parameters into the path and return the body, if it exists.
+    #
+    # @param parameters [Array] The list of parameters to inject into the
+    #   path.
+    # @raise [ArgumentError] Raised if either too many or too few parameters
+    #   were provided.
+    # @return [String,Object] A path and request body pair.  The body value is
+    #   nil if a payload wasn't included in the list of parameters.
     def format_path(parameters)
       parameter_regex = /\{\(\#[\/a-zA-Z0-9]*\)\}/
       parameter_size = @path.scan(parameter_regex).size
+      too_few_parameters = parameter_size > parameters.size
+      too_many_parameters = parameter_size < (parameters.size - 1)
+      if too_few_parameters || too_many_parameters
+        raise ArgumentError.new("wrong number of arguments " +
+                                "(#{parameters.size} for #{parameter_size})")
+      end
       path = @path
       (0..parameter_size).each do |i|
         path = path.sub(parameter_regex, format_parameter(parameters[i]))
       end
-      remaining_parameters = parameters.slice(parameter_size)
-      body = remaining_parameters.first if remaining_parameters
+      body = parameters.slice(parameter_size)
       return path, body
     end
 
+    # Convert a path parameter to a format suitable for use in a path.
+    #
+    # @param [Fixnum,String,TrueClass,FalseClass,Time] The parameter to format.
+    # @return [String] The formatted parameter.
     def format_parameter(parameter)
       parameter.instance_of?(Time) ? iso_format(parameter) : parameter.to_s
     end
--- test/link_test.rb
+++ test/link_test.rb
@@ -8,10 +8,10 @@ class LinkTest < MiniTest::Test
     @url = 'https://username:secret@example.com'
   end
 
-  # Link.run invokes a GET request against the service identified by the URL.
-  # The path is unchanged when no parameters aren't required and the username
-  # and password are passed using HTTP basic auth.
-  def test_run_get_request_without_arguments_and_empty_response
+  # Link.run invokes a request against the service identified by the URL.  The
+  # path is left unchanged when parameters aren't required and the username
+  # and password from the URL are passed using HTTP basic auth.
+  def test_run_without_parameters_and_with_empty_response
     link = Heroics::Link.new(@url, '/resource', :get)
     Excon.stub(method: :get) do |request|
       assert_equal('Basic dXNlcm5hbWU6c2VjcmV0',
@@ -26,7 +26,7 @@ class LinkTest < MiniTest::Test
   end
 
   # Link.run injects parameters into the path in the order they were received.
-  def test_run_get_request_with_arguments_and_empty_response
+  def test_run_with_parameters_and_empty_response
     uuid = 'a72139f8-7737-4def-9e0f-19b6291a93d2'
     path = '/resource/{(#/bool)}/{(#/date)}/{(#/int)}/{(#/string)}/{(#/uuid)}'
     link = Heroics::Link.new(@url, path, :get)
@@ -39,9 +39,26 @@ class LinkTest < MiniTest::Test
     assert_equal(nil, link.run(true, Time.utc(2013), 42, 'hello', uuid))
   end
 
+  # Link.run optionally takes an extra parameter to send in the request body.
+  def test_run_without_parameters_and_with_request_body
+    body = {'Hello' => 'world!'}
+    link = Heroics::Link.new(@url, '/resource', :post)
+    Excon.stub(method: :post) do |request|
+      assert_equal('Basic dXNlcm5hbWU6c2VjcmV0',
+                   request[:headers]['Authorization'])
+      assert_equal('example.com', request[:host])
+      assert_equal(443, request[:port])
+      assert_equal('/resource', request[:path])
+      assert_equal(body, MultiJson.load(request[:body]))
+      Excon.stubs.pop
+      {status: 200, body: ''}
+    end
+    assert_equal(nil, link.run(body))
+  end
+
   # Link.run returns text responses sent by the server without processing them
   # in any way.
-  def test_run_get_request_with_text_response
+  def test_run_with_text_response
     link = Heroics::Link.new(@url, '/resource', :get)
     Excon.stub(method: :get) do |request|
       assert_equal('Basic dXNlcm5hbWU6c2VjcmV0',
@@ -51,8 +68,65 @@ class LinkTest < MiniTest::Test
       assert_equal('/resource', request[:path])
       Excon.stubs.pop
       {status: 200, headers: {'Content-Type' => 'application/text'},
-       body: 'Hello, world!'}
+       body: "Hello, world!\r\n"}
     end
-    assert_equal('Hello, world!', link.run)
+    assert_equal("Hello, world!\r\n", link.run)
+  end
+
+  # Link.run automatically decodes JSON responses sent by the server into Ruby
+  # objects.
+  def test_run_with_json_response
+    body = {'Hello' => 'World!'}
+    link = Heroics::Link.new(@url, '/resource', :get)
+    Excon.stub(method: :get) do |request|
+      assert_equal('Basic dXNlcm5hbWU6c2VjcmV0',
+                   request[:headers]['Authorization'])
+      assert_equal('example.com', request[:host])
+      assert_equal(443, request[:port])
+      assert_equal('/resource', request[:path])
+      Excon.stubs.pop
+      {status: 200, headers: {'Content-Type' => 'application/json'},
+       body: MultiJson.dump(body)}
+    end
+    assert_equal(body, link.run)
+  end
+
+  # Link.run automatically decodes JSON responses with a complex Content-Type
+  # header sent by the server into Ruby objects.
+  def test_run_with_json_response_and_complex_content_type
+    body = {'Hello' => 'World!'}
+    link = Heroics::Link.new(@url, '/resource', :get)
+    Excon.stub(method: :get) do |request|
+      assert_equal('Basic dXNlcm5hbWU6c2VjcmV0',
+                   request[:headers]['Authorization'])
+      assert_equal('example.com', request[:host])
+      assert_equal(443, request[:port])
+      assert_equal('/resource', request[:path])
+      Excon.stubs.pop
+      {status: 200,
+       headers: {'Content-Type' => 'application/json;charset=utf-8'},
+       body: MultiJson.dump(body)}
+    end
+    assert_equal(body, link.run)
+  end
+
+  # Link.run raises an ArgumentError if too few parameters are provided.
+  def test_run_with_missing_parameters
+    path = '/resource/{(#/parameter)}'
+    link = Heroics::Link.new(@url, path, :get)
+    error = assert_raises ArgumentError do
+      link.run
+    end
+    assert_equal('wrong number of arguments (0 for 1)', error.message)
+  end
+
+  # Link.run raises an ArgumentError if too many parameters are provided.
+  def test_run_with_too_many_parameters
+    path = '/resource/{(#/parameter)}'
+    link = Heroics::Link.new(@url, path, :get)
+    error = assert_raises ArgumentError do
+      link.run('too', 'many', 'parameters')
+    end
+    assert_equal('wrong number of arguments (3 for 1)', error.message)
   end
 end
