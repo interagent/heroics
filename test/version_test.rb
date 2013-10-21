af017e171cfd8c4b1425303f173e2148c49bd9e8
Jamu Kakar <jkakar@kakar.ca>
2013-10-20 23:05:15 -0700

    - Improve docstring.

--- test/version_test.rb
+++ test/version_test.rb
@@ -1,7 +1,7 @@
 require 'helper'
 
 class VersionTest < MiniTest::Test
-  # Heroics::VERSION defines version for the project in MAJOR.MINOR.PATCH
+  # Heroics::VERSION defines the version for the project in MAJOR.MINOR.PATCH
   # format.
   def test_version
     assert_match(/\d+.\d+.\d+/, Heroics::VERSION)
