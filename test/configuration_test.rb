2b7be14ac1fe888ad5037ceab8cc834d2e7ab5dc
Matt Gauger <matt.gauger@gmail.com>
2017-02-03 13:42:52 -0600

    Fix tests that were not being picked up

--- test/configuration_test.rb
+++ test/configuration_test.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
+require 'heroics/configuration'
+
 class ConfigurationTest < MiniTest::Unit::TestCase
-  def yields_itself_on_new_if_block_provided
+  def test_yields_itself_on_new_if_block_provided
     yielded_object = nil
-    config = Configuration.new { |c| yielded_object = c }
+    config = Heroics::Configuration.new { |c| yielded_object = c }
     assert_equal(yielded_object, config)
   end
 
--- test/heroics_test.rb
+++ test/heroics_test.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
+require 'heroics'
+
 class HeroicsTest < MiniTest::Unit::TestCase
   def test_default_configuration
     assert_equal(Heroics.default_configuration.class, Heroics::Configuration)
   end
 
-  def yields_configuration_if_block_given
+  def test_yields_configuration_if_block_given
     yielded = nil
 
     Heroics.default_configuration { |c| yielded = c }
